ce2c2bd7735fcbff7ac03d2f0085a413d5dc9fe3
Randi K <randiquek@gmail.com>
2025-03-18 10:20:43 -0400

    generated file for spring

---
+++ gamediscoveryapp/src/main/java/com/example/gamediscoveryapp/GamediscoveryappApplication.java
@@ -0,0 +1,13 @@
+package com.example.gamediscoveryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamediscoveryappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamediscoveryappApplication.class, args);
+	}
+
+}
---
+++ gamediscoveryapp/src/test/java/com/example/gamediscoveryapp/GamediscoveryappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gamediscoveryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamediscoveryappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
